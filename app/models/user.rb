3849857736d28db631ce5d893a640e0cbe9aae19
savanna chiang <savannaac@gmail.com>
2021-04-24 23:46:29 -0500

    creates models, activerecord associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :photos
+  has_many :liked_photos, through: :likes, source: photo
 end
