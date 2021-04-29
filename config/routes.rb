9e617d89a17bf5bd800f68b522618f66d319d96a
savanna chiang <savannaac@gmail.com>
2021-04-29 00:00:31 -0500

    creates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "photos#index"
+  
+  resources :users, only: [:show] do
+    resources :photos, only: [:show, :index]
+  end
+
+  resources :photos, only: [:index, :show, :new, :create, :edit, :update]
 end
