7b226d660347baa9e15fad6df46249620b2bb08c
savanna chiang <savannaac@gmail.com>
2021-04-30 22:27:25 -0500

    adds photo validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,7 @@ class Photo < ApplicationRecord
      belongs_to :user
      has_many :likes
      has_many :users_who_liked, through: :likes, source: :user
+
+     validates :img_url, presence: true
+     validates :public, presence: true 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :users, only: [:show] do
+  root "application#home"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/signup", to: "users#new"
+
+  resources :users, except: [:new] do
     resources :photos, only: [:index, :new, :show]
   end
 
